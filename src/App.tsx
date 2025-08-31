3fe61900f5ec680f223c4a348f8c52bdd9839966
digeon-ito <s-ito@digeon.co>
2025-08-31 12:02:40 +0900

    add 最初の画面

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { FaBeer } from 'react-icons/fa'
+import NamesCenteredGradient from "./WIndow.tsx";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50 text-gray-800">
-      <h1 className="mb-4 text-3xl font-bold">Vite + React + Tailwind</h1>
-      <p className="flex items-center gap-2 text-lg">
-        Enjoy a cold one <FaBeer />
-      </p>
-    </div>
+    <NamesCenteredGradient />
   )
 }
 
